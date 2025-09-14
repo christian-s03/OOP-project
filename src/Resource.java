520c927eb0fc6419652ad1c8d6512ab8facd1308
christian-s03 <suskikrystian8@gmail.com>
2025-09-14 20:02:21 +0200

    TASK 3.4

---
+++ src/Resource.java
@@ -0,0 +1,42 @@
+public abstract class Resource {
+    private String name;
+    private Money customHourlyRate;
+
+    public Resource(String name, Money customHourlyRate) {
+        this.name = name;
+        this.customHourlyRate = customHourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Money getCustomHourlyRate() {
+        return customHourlyRate;
+    }
+
+    public void setCustomHourlyRate(Money customHourlyRate) {
+        this.customHourlyRate = customHourlyRate;
+    }
+
+    protected abstract Money baseRatePerHour();
+
+    public abstract String describe();
+
+    public Money hourlyRate() {
+        if (customHourlyRate != null) {
+            return customHourlyRate;
+        } else {
+            return baseRatePerHour();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return describe();
+    }
+}
